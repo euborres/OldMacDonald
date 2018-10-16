ed7fe34515bd38a8bbf0c3758e0f113219056653
euborres <euborres@gmail.com>
2018-10-16 13:35:09 -0700

    Added NamedCow class

--- Chick.java
+++ Chick.java
@@ -10,6 +10,16 @@ class Chick implements Animal
    myType = "unknown";
    mySound = "unknown";
   }
+  public Chick(String type, String sound, String sound2){
+    myType = type;
+    int random = (int)(Math.random() * 2);
+    if(random < 1){
+       mySound = sound; 
+    }
+    else{
+        mySound = sound2;
+    }
+  }
   public String getSound(){return mySound;}
   public String getType(){return myType;}
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -2,10 +2,8 @@
 void setup()
 {
 	//your variable declarations and
-  Farm[] hi = new Farm [3];
-  for(int i = 0; i < hi.length; i++){
-   hi[i] = new animalSounds(); 
-  }
+  Farm hi = new Farm();
+  hi.animalSounds();
 	//System.out.println() statements here
 
 }
